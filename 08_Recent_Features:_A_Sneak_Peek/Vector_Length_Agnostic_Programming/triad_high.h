c78281e0ec6d97af1ec71de1e14ead2b47a69928
kroppel <konsti.roppel@t-online.de>
2022-06-06 11:36:28 +0200

    Added triad high cpp file and header

---
+++ 08_Recent_Features:_A_Sneak_Peek/Vector_Length_Agnostic_Programming/triad_high.h
@@ -0,0 +1,7 @@
+#ifndef TRIAD_HIGH
+#define TRIAD_HIGH
+void triad_high( uint64_t         i_nValues,
+                 float    const * i_a,
+                 float    const * i_b,
+                 float          * o_c );
+#endif
