fb14dced4432945cfe3857e6e7457953a0487dde
kroppel <konsti.roppel@t-online.de>
2022-06-06 13:17:59 +0200

    Header fix first try

--- 08_Recent_Features:_A_Sneak_Peek/Vector_Length_Agnostic_Programming/triad_high.h
+++ 08_Recent_Features:_A_Sneak_Peek/Vector_Length_Agnostic_Programming/triad_high.h
@@ -1,7 +1,6 @@
-#ifndef TRIAD_HIGH
-#define TRIAD_HIGH
-void triad_high( uint64_t         i_nValues,
-                 float    const * i_a,
-                 float    const * i_b,
-                 float          * o_c );
+#ifndef __TRIAD_HIGH__
+#define __TRIAD_HIGH__
+
+void triad_high(uint64_t i_nValues, float const * i_a, float const * i_b, float * o_c );
+
 #endif
