6241520eca534e696a06a1447fa7a5e503c125c2
kroppel <konsti.roppel@t-online.de>
2022-06-07 12:03:28 +0200

    added kernel fmlalx.s

--- 08_Recent_Features:_A_Sneak_Peek/SVE2/fmlalx.s
+++ 08_Recent_Features:_A_Sneak_Peek/SVE2/fmlalx.s
@@ -4,7 +4,14 @@
         .global fmlalx
 
 fmlalx:
-        
+        ldr z0, [x0]
+        ldr z1, [x1]
+        ldr z2, [x2]
+
+        fmlalb z2.s, z1.h, z0.h
+        fmlalt z2.s, z1.h, z0.h
+
+        str z2, [x2]
 
         ret
         .size fmlalx, (. - fmlalx)
