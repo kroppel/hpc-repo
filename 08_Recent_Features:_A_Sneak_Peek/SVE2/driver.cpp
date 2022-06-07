840ff631df1fffc38f2fc59d69e8ba1822f28411
kroppel <konsti.roppel@t-online.de>
2022-06-07 10:54:35 +0200

    Added test 1 and 3 for eor3 to driver

--- 08_Recent_Features:_A_Sneak_Peek/SVE2/driver.cpp
+++ 08_Recent_Features:_A_Sneak_Peek/SVE2/driver.cpp
@@ -4,39 +4,75 @@
 #include <iostream>
 #include <cmath>
 #include <chrono>
+#include <cstring>
+
 
 extern "C" {
-    void fmlalb(      float    const * i_a,
+    void fmlalb_fmlalt(      float    const * i_a,
                 float    const * i_b,
                 float          * o_c );
-    void fmlalt(      float    const * i_a,
-                float    const * i_b,
-                float          * o_c );
-    void eor(      float    const * i_a,
-                float    const * i_b,
-                float          * o_c );
+    void eor3( unsigned int  * i_a,
+               unsigned int  * i_b,
+               unsigned int  * o_c );
 }
 
-int main() {
-
+void example_fmlalt() {
     //Vectors A, B, C
-    float const l_a[32] = {0};
-    float const l_b[32] = {0};
+    float l_a[32] = {0};
+    float l_b[32] = {0};
     float l_c[16] = {0};
 
-    for (int i = 0; i < 32; i++) {
-        l_a = i;
-        l_b = i;
-    }
 
-    fmlalb(l_a, l_b, l_c);
+    fmlalb_fmlalt(l_a, l_b, l_c);
 
     std::cout << "C: ";
     for (int i = 0; i < 32; i++) {
         std::cout << i << ", ";
     } 
     std::cout << "\n";
+}
 
-    return EXIT_SUCCESS;    
+void example_eor() {
+    // Test 1
+    unsigned int l_a1[16] = {0xFFFFFFFF};
+    unsigned int l_b1[16] = {0xFFFFFFFF};
+    unsigned int * l_c1 = (unsigned int *) malloc(16*4);
+    unsigned int * l_c_exp1 = (unsigned int *) malloc(16*4); 
+    memcpy(l_c_exp1, l_c1, 16*4);
+
+    eor3(l_a1, l_b1, l_c1);
+
+    bool equal = true;
+    for (int i = 0; i < 16; i++) {
+        if (l_c1[i] != l_c_exp1[i]) {
+            equal = false;
+        }
+    }
+
+    free(l_c1);
+
+    assert(equal && "Test 1 failed");
+
+    // Test 3
+    unsigned int l_a2[16]     = {0x0F0F0F0F};
+    unsigned int l_b2[16]     = {0x33333333};
+    unsigned int l_c2[16]     = {0x55555555};
+    unsigned int l_c_exp2[16] = {0x69696969};
+
+    eor3(l_a2, l_b2, l_c2);
+
+    equal = true;
+    for (int i = 0; i < 16; i++) {
+        if (l_c2[i] != l_c_exp2[i]) {
+            equal = false;
+        }
+    }
+
+    assert(equal && "Test 3 failed");
+}
+
+int main() {
+
+    example_eor(); 
 }
 
