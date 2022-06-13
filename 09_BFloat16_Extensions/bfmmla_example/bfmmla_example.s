6fffb2ffb8ca51527f8572537166ce132add1ce2
kroppel <konsti.roppel@t-online.de>
2022-06-13 12:41:12 +0200

    Finished kernel buggy

--- 09_BFloat16_Extensions/bfmmla_example/bfmmla_example.s
+++ 09_BFloat16_Extensions/bfmmla_example/bfmmla_example.s
@@ -12,15 +12,17 @@
          * @param x3 pointer to C
          */ 
 bfmmla_example:
-        ptrue p0.b
+        ptrue p0.h, VL16
+        ptrue p1.h, VL8
+        ptrue p2.s, VL8
 
-        ld1w {z0}, p0/z, [x0]
-        ld1w {z1}, p0/z, [x2]
-        ld1w {z2}, p0/z, [x2]
+        ld1w {z0.s}, p0/z, [x0]
+        ld1w {z1.s}, p1/z, [x2]
+        ld1w {z2.s}, p2/z, [x2]
 
-        bfmmla 
+        bfmmla z2.s, z0.h, z1.h
 
-        st1w {z2}, p0, [x2]
+        st1w {z2.s}, p2, [x2]
         
         ret
         .size bfmmla_example, (. - bfmmla_example)
