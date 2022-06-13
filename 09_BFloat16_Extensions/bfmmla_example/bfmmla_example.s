3b7c8ce4a71ee70bd6dd203de54f3f13118a676f
kroppel <konsti.roppel@t-online.de>
2022-06-13 14:24:40 +0200

    Small changes, code should work but was tested on the wrong machine

--- 09_BFloat16_Extensions/bfmmla_example/bfmmla_example.s
+++ 09_BFloat16_Extensions/bfmmla_example/bfmmla_example.s
@@ -1,5 +1,4 @@
         .text
-        .align 4
         .type bfmmla_example, %function
         .global bfmmla_example
 
@@ -12,18 +11,26 @@
          * @param x3 pointer to C
          */ 
 bfmmla_example:
-        ptrue p0.h, VL16
-        ptrue p1.h, VL8
+        ptrue p0.b
+
+        /*ptrue p0.h, VL16
+        ptrue p1.h, VL16
         ptrue p2.s, VL8
 
         ld1w {z0.s}, p0/z, [x0]
-        ld1w {z1.s}, p1/z, [x2]
-        ld1w {z2.s}, p2/z, [x2]
+        ld1w {z1.s}, p1/z, [x1] 
+        ld1w {z2.s}, p2/z, [x2]*/
+
+        ldr z0, [x0]
+        ldr z1, [x1]
+        ld1 z2, [x2]
+
 
         bfmmla z2.s, z0.h, z1.h
 
-        st1w {z2.s}, p2, [x2]
-        
+        str z2, [x2]
+
+        //st1w {z2.s}, p2, [x2]
         ret
         .size bfmmla_example, (. - bfmmla_example)
 
