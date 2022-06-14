d0069017dce4c42580134f6ce9cffcce223352b1
kroppel <konsti.roppel@t-online.de>
2022-06-14 09:03:01 +0000

    Removed duplicate drivers

--- 09_BFloat16_Extensions/bfmmla_example/bfmmla_example.s
+++ 09_BFloat16_Extensions/bfmmla_example/bfmmla_example.s
@@ -11,26 +11,17 @@
          * @param x3 pointer to C
          */ 
 bfmmla_example:
-        ptrue p0.b
-
-        /*ptrue p0.h, VL16
+        ptrue p0.h, VL16
         ptrue p1.h, VL16
         ptrue p2.s, VL8
 
         ld1w {z0.s}, p0/z, [x0]
         ld1w {z1.s}, p1/z, [x1] 
-        ld1w {z2.s}, p2/z, [x2]*/
-
-        ldr z0, [x0]
-        ldr z1, [x1]
-        ld1 z2, [x2]
-
+        ld1w {z2.s}, p2/z, [x2]
 
         bfmmla z2.s, z0.h, z1.h
 
-        str z2, [x2]
-
-        //st1w {z2.s}, p2, [x2]
+        st1w {z2.s}, p2, [x2]
         ret
         .size bfmmla_example, (. - bfmmla_example)
 
